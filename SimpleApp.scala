f94f39be6dfbf7a1f7589bf5eab503257645c440
Manish Ranjan <cse1.manish@gmail.com>
2017-03-19 19:23:23 -0400

    Added bare bone scala app to run on spark

---
+++ SimpleApp.scala
@@ -0,0 +1,11 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SimpleApp{
+        def main(args: Array[String]){
+                println("Say hello From Scala")
+            
+            }
+    
+    }
